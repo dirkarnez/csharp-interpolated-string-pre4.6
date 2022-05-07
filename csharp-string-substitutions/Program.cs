6b333e72ae6b3d19b458d77e82728ecf577935d9
dirkarnez <smalldirkalex@gmail.com>
2022-05-08 01:48:34 +0800

    - redo using regex

--- csharp-string-substitutions/Program.cs
+++ csharp-string-substitutions/Program.cs
@@ -1,54 +1,86 @@
 ﻿using System;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace csharp_interpolated_string_pre4._6
 {
-    class Job
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+    //class Job
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Description { get; set; }
 
-        public Customer[] Customers { get; set; }
-    }
+    //    public Customer[] Customers { get; set; }
+    //}
 
-    class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    //class Customer
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-            var job = new Job
+            var a = "{{   b      }}";
+            var b = "world";
+            var template = "this is a string with variable {{    a   }} and {{b}}, again {{a}} {{b}}";
+
+            MatchEvaluator matcher = match =>
             {
-                Id = 1,
-                Name = "Todo",
-                Description = "{Job.Name}",
-                Customers = new Customer[]
+                // DisplayMatchResults(match);
+                
+                if (match.Groups[1].Value.Trim() == "a")
                 {
-                    new Customer
-                    {
-                        Name = "{Job.Name}"
-                    }
+                    return a;
                 }
+                else if (match.Groups[1].Value.Trim() == "b")
+                {
+                    return b;
+                }
+                return "";
             };
 
-            Console.WriteLine(ReplaceMacro("{Job.Name} {Job.Description} { Job.Customers[0].Name } job for admin", job));
+            Console.WriteLine(Regex.Replace(template, "{{([^{}]+)}}", matcher));
             Console.ReadLine();
         }
 
-        public static string ReplaceMacro(string value, object @object)
+        public static void DisplayMatchResults(Match match)
         {
-            return Regex.Replace(value, @"{(.+?)}",
-            match => {
-                var p = Expression.Parameter(@object.GetType(), @object.GetType().Name);
-                var e = System.Linq.Dynamic.DynamicExpression.ParseLambda(new[] { p }, null, match.Groups[1].Value);
-                return (e.Compile().DynamicInvoke(@object) ?? "").ToString();
-            });
+            Console.WriteLine("Match has {0} captures", match.Captures.Count);
+
+            int groupNo = 0;
+            foreach (Group mm in match.Groups)
+            {
+                Console.WriteLine("  Group {0,2} has {1,2} captures '{2}'", groupNo, mm.Captures.Count, mm.Value);
+
+                int captureNo = 0;
+                foreach (Capture cc in mm.Captures)
+                {
+                    Console.WriteLine("       Capture {0,2} '{1}'", captureNo, cc);
+                    captureNo++;
+                }
+                groupNo++;
+            }
+
+            groupNo = 0;
+            foreach (Group mm in match.Groups)
+            {
+                Console.WriteLine("    match.Groups[{0}].Value == \"{1}\"", groupNo, match.Groups[groupNo].Value); //**
+                groupNo++;
+            }
+
+            groupNo = 0;
+            foreach (Group mm in match.Groups)
+            {
+                int captureNo = 0;
+                foreach (Capture cc in mm.Captures)
+                {
+                    Console.WriteLine("    match.Groups[{0}].Captures[{1}].Value == \"{2}\"", groupNo, captureNo, match.Groups[groupNo].Captures[captureNo].Value); //**
+                    captureNo++;
+                }
+                groupNo++;
+            }
         }
     }
 }
